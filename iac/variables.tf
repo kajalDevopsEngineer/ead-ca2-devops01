adedd6991fbfba73208938862d82b9ae5ad2cc15
kajalDevopsEngineer <kajaldevopsudemy@gmail.com>
2026-04-21 19:00:01 +0100

    IAC implementation

---
+++ iac/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "rg-ead-ca201"
+}
+
+variable "location" {
+  default = "uksouth"
+}
+
+variable "aks_name" {
+  default = "aks-ead-cluster"
+}
+
+variable "acr_name" {
+  default = "acreaadca201"
+}
